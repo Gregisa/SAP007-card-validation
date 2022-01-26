470d760e91e36601c3d6da8727a85df5222805a7
IsabelaGregoraci <isabela.bonander@gmail.com>
2022-01-25 23:30:33 -0300

    alterações js e html

--- src/validator.js
+++ src/validator.js
@@ -17,46 +17,46 @@ document.addEventListener("DOMContentLoaded", function() {
       /*array feito a partir do número digitado*/
      const cardArray = numeroDigitado.split('');
      /*parseInt p transformar strings em números e assim conseguir fazer operações */
-      cardArray[15] = parseInt(cardArray[15]) * 2;
+      cardArray[1] = parseInt(cardArray[1]) * 2;
       if(parseInt(cardArray[1])>=10){
-        cardArray[15] = String(cardArray[15]).split('');
+        cardArray[1] = String(cardArray[1]).split('');
         /*split é uma expressão regular p  */
-        cardArray[15] = parseInt(cardArray[15][0]) + parseInt(cardArray[15][1]);
+        cardArray[1] = parseInt(cardArray[1][0]) + parseInt(cardArray[1][1]);
     }
-      cardArray[13] = parseInt(cardArray[13]) * 2;
+      cardArray[3] = parseInt(cardArray[3]) * 2;
       if(parseInt(cardArray[13])>=10){
-        cardArray[13] = String(cardArray[13]).split('');
-        cardArray[13] = parseInt(cardArray[13][0]) + parseInt(cardArray[13][1]);
+        cardArray[3] = String(cardArray[3]).split('');
+        cardArray[3] = parseInt(cardArray[3][0]) + parseInt(cardArray[3][1]);
     }
-      cardArray[11] = parseInt(cardArray[11]) * 2;
+      cardArray[5] = parseInt(cardArray[5]) * 2;
         if(parseInt(cardArray[5])>=10){
-          cardArray[11] = String(cardArray[11]).split('');
-          cardArray[11] = parseInt(cardArray[11][0]) + parseInt(cardArray[11][1]);
+          cardArray[5] = String(cardArray[5]).split('');
+          cardArray[5] = parseInt(cardArray[5][0]) + parseInt(cardArray[5][1]);
       }
-      cardArray[9] = parseInt(cardArray[9]) * 2;
-      if(parseInt(cardArray[5])>=10){
-        cardArray[9] = String(cardArray[9]).split('');
-        cardArray[9] = parseInt(cardArray[9][0]) + parseInt(cardArray[9][1]);
-    }
       cardArray[7] = parseInt(cardArray[7]) * 2;
       if(parseInt(cardArray[5])>=10){
         cardArray[7] = String(cardArray[7]).split('');
         cardArray[7] = parseInt(cardArray[7][0]) + parseInt(cardArray[7][1]);
     }
-      cardArray[5] = parseInt(cardArray[5]) * 2;
+      cardArray[9] = parseInt(cardArray[9]) * 2;
       if(parseInt(cardArray[5])>=10){
-        cardArray[5] = String(cardArray[5]).split('');
-        cardArray[5] = parseInt(cardArray[5][0]) + parseInt(cardArray[5][1]);
+        cardArray[9] = String(cardArray[9]).split('');
+        cardArray[9] = parseInt(cardArray[9][0]) + parseInt(cardArray[9][1]);
     }
-      cardArray[3] = parseInt(cardArray[3]) * 2;
+      cardArray[11] = parseInt(cardArray[11]) * 2;
+      if(parseInt(cardArray[11])>=10){
+        cardArray[11] = String(cardArray[11]).split('');
+        cardArray[11] = parseInt(cardArray[11][0]) + parseInt(cardArray[11][1]);
+    }
+      cardArray[13] = parseInt(cardArray[13]) * 2;
       if(parseInt(cardArray[3])>=10){
-        cardArray[3] = String(cardArray[3]).split('');
-        cardArray[3] = parseInt(cardArray[3][0]) + parseInt(cardArray[3][1]);
+        cardArray[13] = String(cardArray[13]).split('');
+        cardArray[13] = parseInt(cardArray[13][0]) + parseInt(cardArray[13][1]);
     }
-      cardArray[1] = parseInt(cardArray[1]) * 2;
+      cardArray[15] = parseInt(cardArray[15]) * 2;
       if(parseInt(cardArray[5])>=10){
-        cardArray[1] = String(cardArray[1]).split('');
-        cardArray[1] = parseInt(cardArray[1][0]) + parseInt(cardArray[1][1]);
+        cardArray[15] = String(cardArray[15]).split('');
+        cardArray[15] = parseInt(cardArray[15][0]) + parseInt(cardArray[15][1]);
     }
       console.log(cardArray);
   });
