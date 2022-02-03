f63ae4987c92ecb32ec1af373761ff9e30b779ad
IsabelaGregoraci <isabela.bonander@gmail.com>
2022-02-03 17:51:51 -0300

    atualizaçoes js e html

--- src/validator.js
+++ src/validator.js
@@ -7,14 +7,14 @@ const validator = {                            //validator armazena as função 
       cardArray.push(digit)              //caracteres do array transformados em numbers através do parseInt
     }                                       //'push' manda para a variável 'cardArray' seus caracteres já transformados em numbers                                   //em 'arrayNumbers'
   
-    let reverseArray=cardArray.reverse() 
-     //'reverseArray' armazena o resultado dos números do array invertidos por meio do reverse()                                           
+    let reverseArray=cardArray.reverse()        //'reverseArray' armazena o resultado dos números do array invertidos por meio do reverse()
+                                                                            
     for(let i=0; i<reverseArray.length; i++){   //for identfica as posições intercaladas dentro do array que serão *2
-      if(i%2===0){
-        reverseArray[i]=reverseArray[i]*2
+      if((i+1)%2===0){                            //se a posição dentro do array for par, multiplicar por 2
+        reverseArray[i]=reverseArray[i]*2       //então o número identificado em reverse array[i] agr se torna ele mesmo dobrado
       }
-      if(reverseArray[i]>=10){
-        reverseArray[i] = reverseArray[i]-9
+      if(reverseArray[i]>=10){                  //se o número dobrado for>=10
+        reverseArray[i] = reverseArray[i]-9     //deverá ser subtraído por 9
           console.log(reverseArray);
       }
     }
