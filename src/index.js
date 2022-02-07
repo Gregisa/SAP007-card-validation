0b3cdfd5c13bdad2b752ee86b21a86c60e3b2bd8
IsabelaGregoraci <isabela.bonander@gmail.com>
2022-02-07 17:30:54 -0300

    alteraçoes js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import validator from './validator.js';
 
 const button = document.getElementById('cardButton');      //a variável 'button' se refere ao botão Validar
 const cardNumber =document.getElementById('cardNumber');  //a variável 'cardNumber' e refere ao número digitado
-const mask= validator.maskify(cardNumber.masked);                //chamando maskify com 
 const digitsValue= cardNumber.value;                     //'digitsValue' pega o valor do número digitado no input 'cardNumber'
 button.addEventListener('click', cardValidation);      //o escutador de eventos "ouve" o click de button(var de 'cardButton') e desencadeia a ação                                                    
 
 function cardValidation() {                            //no caso, a função 'cardValidation'
- 
+
+  /*const mask= validator.maskify(cardNumber).value;
+  document.getElementById('cardNumber').value*/ 
+
   const finalValidation = validator.isValid(digitsValue);     //veredicto da validação, onde 'finalValidation' checa se a var 'validator' aplicada à
        
       if (digitsValue.length===0 || digitsValue===''); {    //caso o valor digitado em 'cardNumber seja zero ou uma string, disparar alert
