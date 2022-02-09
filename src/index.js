8db1e8d7974e7686e7622b1174477ca0aecafb5b
IsabelaGregoraci <isabela.bonander@gmail.com>
2022-02-09 13:01:47 -0300

    validaçao&alerts OK, alteraçoes layout

--- src/index.js
+++ src/index.js
@@ -2,27 +2,29 @@ import validator from './validator.js';
 
 const button = document.getElementById('cardButton');      //a variável 'button' se refere ao botão Validar
 const cardNumber =document.getElementById('cardNumber');  //a variável 'cardNumber' e refere ao número digitado
-const digitsValue= cardNumber.value;                     //'digitsValue' pega o valor do número digitado no input 'cardNumber'
 button.addEventListener('click', cardValidation);      //o escutador de eventos "ouve" o click de button(var de 'cardButton') e desencadeia a ação                                                    
 
 function cardValidation() { 
+  const digitsValue= cardNumber.value; 
    
-  const mask= validator.maskify(digitsValue);
-  document.getElementById('cardNumber',).value=mask;
-  document.addEventListener('keyup', mask);                      
+  /*const mask= validator.maskify(digitsValue);
+  document.getElementById('cardNumber').value=mask;
+ document.addEventListener('keyup', mask); criar função mascarar em outro escopo*/                 
 
 
   const finalValidation = validator.isValid(digitsValue);     
 
-      if (digitsValue.length===0 || digitsValue===''); {    
-         alert('Por favor, digite um número de cartão válido')
+      if (digitsValue.length===0 || digitsValue==='') { 
+         console.log(digitsValue.length);   
+          return alert('Por favor, digite um número de cartão válido');
+         
       }
   
       if (finalValidation === true) {                         
-        alert("Cartão válido! Bem vinda, usuária Prèmiere");
+         return alert("Cartão válido! Bem vinda, usuária Prèmiere");
       }
       if(finalValidation===false) {
-        alert("Número de cartão inválido");
+        return alert("Número de cartão inválido");
       }   
 }
   
